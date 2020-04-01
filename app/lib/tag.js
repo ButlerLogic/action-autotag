dcbf76b3cc067f04f3e98b4417d0d5ca73254f5e
Corey Butler <corey@coreybutler.com>
2020-04-01 13:32:03 -0500

    Update imports

--- app/lib/tag.js
+++ app/lib/tag.js
@@ -1,9 +1,9 @@
 import core from '@actions/core'
 import os from 'os'
-import GitHub from '@actions/github'
+import gh from '@actions/github'
 
 // Get authenticated GitHub client (Ocktokit): https://github.com/actions/toolkit/tree/master/packages/github#usage
-const github = new GitHub(process.env.GITHUB_TOKEN || process.env.INPUT_GITHUB_TOKEN)
+const github = new gh.GitHub(process.env.GITHUB_TOKEN || process.env.INPUT_GITHUB_TOKEN)
 const context = github.context
 // Get owner and repo from context of payload that triggered the action
 const { owner, repo } = context.repo
