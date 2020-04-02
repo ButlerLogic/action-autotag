887f931751eabb86d38576ceccf67a39d8a6a926
Corey Butler <corey@coreybutler.com>
2020-04-02 16:49:51 -0500

    Update Docker debugging.

--- app/lib/docker.js
+++ app/lib/docker.js
@@ -5,7 +5,7 @@ import fs from 'fs'
 export default class Dockerfile extends Regex {
   constructor (root = null) {
     root = path.join(process.env.GITHUB_WORKSPACE, root)
-
+    core.debug(fs.readdirSync(root))
     if (fs.statSync(root).isDirectory()) {
       root = path.join(root, 'Dockerfile')
     }
