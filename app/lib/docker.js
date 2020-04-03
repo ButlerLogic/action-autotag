dec8dcd1af4084079c8a00ee9cb60b606c30e865
Corey Butler <corey@coreybutler.com>
2020-04-02 23:19:41 -0500

    WIP

--- app/lib/docker.js
+++ app/lib/docker.js
@@ -1,11 +1,12 @@
 import Regex from './regex.js'
 import path from 'path'
 import fs from 'fs'
+import core from '@actions/core'
 
 export default class Dockerfile extends Regex {
   constructor (root = null) {
     root = path.join(process.env.GITHUB_WORKSPACE, root)
-
+    core.debug(fs.readdirSync(root))
     if (fs.statSync(root).isDirectory()) {
       root = path.join(root, 'Dockerfile')
     }
